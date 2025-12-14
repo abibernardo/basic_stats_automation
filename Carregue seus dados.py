c37baecfae1d4ead6f2aa82063ffb22163d500b6
abibernardo <153117023+abibernardo@users.noreply.github.com>
2025-12-14 11:58:23 -0300

    Update and rename st_stat_automation.py to Carregue seus dados.py

--- Carregue seus dados.py
+++ Carregue seus dados.py
@@ -65,7 +65,7 @@ st.markdown("""
 
 # --- Conteúdo visual ---
 st.markdown(
-    "<div class='main-title'>Aplicação Interativa para Análise de Dados</div>",
+    "<div class='main-title'>Análise de Dados Automatizada</div>",
     unsafe_allow_html=True
 )
 
@@ -88,7 +88,7 @@ st.markdown("<br><br>", unsafe_allow_html=True)
 st.markdown("""
 <div class='description'>
 Carregue arquivos <strong>Excel</strong> ou <strong>CSV</strong>,
-e faça a análise dos seus dados de forma simples e interativa - sem precisar escrever uma única linha de código!
+e faça a análise dos seus dados de forma simples e interativa, sem precisar escrever uma única linha de código!
 </div>
 """, unsafe_allow_html=True)
 
@@ -151,6 +151,7 @@ with st.container():
 
 if uploaded is not None:
     present_file(uploaded)
+
 
 
 
